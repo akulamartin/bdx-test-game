164c3177cbdbbe91ee2cb12737ca789355f68951
Goran Milovanovic <goran@nilunder.com>
2015-01-15 15:20:56 -0500

    BDX Tutorial - 4 - Programming basic behavior.

--- core/src/com/nilunder/testgame/Player.java
+++ core/src/com/nilunder/testgame/Player.java
@@ -2,13 +2,18 @@ package com.nilunder.testgame;
 
 import com.nilunder.bdx.*;
 
+import javax.vecmath.*;
+
 public class Player extends GameObject{
 
 	public void main(){
 		if (Bdx.mouse.btnHit("left")){
-			GameObject icosphere = scene.add("Bottle");
-			icosphere.position(scene.camera.position());
-			icosphere.velocity(Bdx.mouse.rayDirection().mul(10));
+			GameObject bottle = scene.add("Bottle");
+			bottle.position(scene.camera.position());
+			Vector3f v = Bdx.mouse.rayDirection();
+			v.z += 0.2;
+			v.length(15);
+			bottle.velocity(v);
 		}
 	}
 
--- core/src/com/nilunder/testgame/Sacky.java
+++ core/src/com/nilunder/testgame/Sacky.java
@@ -1,11 +1,27 @@
 package com.nilunder.testgame;
 
 import com.nilunder.bdx.*;
+import com.nilunder.bdx.utils.*;
+
+import javax.vecmath.*;
 
 public class Sacky extends GameObject{
 
 	public void main(){
 		children.get("G_Sacky").orientation(scene.camera.orientation());
+
+		if (hit("Bottle")){
+			jump(Random.direction());
+			touchingObjects.get("Bottle").end();
+		}
+	}
+
+	public void jump(Vector3f direction){
+		Vector3f force = direction;
+		force.z = 0;
+		force.length(100);
+		force.z = 300;
+		applyForce(force);
 	}
     
 }
